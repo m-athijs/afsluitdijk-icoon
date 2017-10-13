288147af76481307b4a2e3aaeb2b05c4fedcc737
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-13 17:28:09 +0200

    i have to test this online

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 export class App {
   constructor() {
     this.message = 'Hello Aurelia on Netlify!';
-    this.message = this.audioDuration;
-    //this.loaded = 100 * this.audioBuffered.end(0) / this.audioDuration;
   }
 }
