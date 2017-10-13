35d9e3283d896a5315834f426cc15bd30467821b
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-13 17:10:09 +0200

    tracking loading of audio

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 export class App {
   constructor() {
     this.message = 'Hello Aurelia on Netlify!';
+    this.message = this.audioDuration;
+    //this.loaded = 100 * this.audioBuffered.end(0) / this.audioDuration;
   }
 }
