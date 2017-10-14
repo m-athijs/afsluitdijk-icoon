0194101784d14cdfa6d10396f5551dfd57d79888
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-14 12:55:32 +0200

    trying the preloader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+import preloader from 'preloader';
+import {inject} from 'aurelia-framework';
+
+// @inject(preloader)
 export class App {
   constructor() {
     this.message = 'Hello Aurelia on Netlify!';
+    this.loaded = 0;
+    // this.loader = loader;
+
+    this.loader = preloader({
+      xhrImages: false
+    });
+    this.loader.add('../assets/audio1.mp3');
+    this.loader.load();
+    this.loader.on('progress',function(progress) {
+      this.loaded = progress * 100;
+      console.log(progress);
+    });
   }
 }
