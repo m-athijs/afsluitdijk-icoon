e19ad036ae25d22df9e0e3ee274c7075289cf20f
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-15 21:10:11 +0200

    fixed the buttons

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import preloader from 'preloader';
 
 export class App {
   constructor() {
+    // this.loaded = false;
   }
 
   startAudio() {
@@ -9,20 +10,27 @@ export class App {
   }
 
   downloadAudio() {
-    this.loaded = 'downloading...';
-    this.loader = preloader({
-      xhrImages: false
-    });
-    this.loader.addAudio('../assets/audio1.mp3');
-    this.loader.load();
-    this.loader.on('progress', function(progress) {
-      console.log(progress);
-      document.getElementById('downloadProgress').innerHTML = ' ' + Math.ceil(progress * 100) + '%';
-    });
-    this.loader.on('complete', function() {
-      this.loaded = 'done';
-      console.log('audio loaded');
-    });
+    if (!this.loaded) {
+      this.loader = preloader({
+        xhrImages: false
+      });
+      this.loader.addAudio('../assets/audio1.mp3');
+      this.loader.load();
+      this.loader.on('progress', function(progress) {
+        console.log(progress);
+        document.getElementById('downloadProgress').innerHTML = ' (' + Math.ceil(progress * 100) + '%)';
+      });
+      this.loader.on('complete', function() {
+        $('#downloadButton').hide();
+        $('#downloadProgress').hide();
+        $('#playButton').show();
+        $('#streamButton').hide();
+        this.loaded = true;
+        console.log('audio loaded');
+      });
+    } else {
+      this.playaudio = true;
+    }
   }
 
 }
