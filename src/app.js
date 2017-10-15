91aab3dea6d693680a69c787eb78915650942a05
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-15 21:26:45 +0200

    cache is a bit of a mystery

--- src/app.js
+++ src/app.js
@@ -1,31 +1,26 @@
 import preloader from 'preloader';
 
 export class App {
-  constructor() {
-    // this.loaded = false;
-  }
-
   startAudio() {
     this.playaudio = true;
   }
-
   downloadAudio() {
     this.loader = preloader({
-        xhrImages: false
-      });
-      this.loader.addAudio('../assets/audio1.mp3');
-      this.loader.load();
-      this.loader.on('progress', function(progress) {
-        console.log(progress);
-        document.getElementById('downloadProgress').innerHTML = ' (' + Math.ceil(progress * 100) + '%)';
-      });
-      this.loader.on('complete', function() {
-        $('#downloadButton').hide();
-        $('#downloadProgress').hide();
-        $('#playButton').show();
-        $('#streamButton').hide();
-        this.loaded = true;
-        console.log('audio loaded');
-      });
+      xhrImages: false
+    });
+    this.loader.addAudio('../assets/audio1.mp3');
+    this.loader.load();
+    this.loader.on('progress', function(progress) {
+      console.log(progress);
+      document.getElementById('downloadProgress').innerHTML = ' (' + Math.ceil(progress * 100) + '%)';
+    });
+    this.loader.on('complete', function() {
+      $('#downloadButton').hide();
+      $('#downloadProgress').hide();
+      $('#playButton').show();
+      $('#streamButton').hide();
+      this.loaded = true;
+      console.log('audio loaded');
+    });
   }
 }
