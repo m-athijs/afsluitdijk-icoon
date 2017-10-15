c1c616306b64724d6426615f5c613f53cd6f851c
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-15 16:11:05 +0200

    A bit of a hack, but it works

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 import preloader from 'preloader';
-import {inject} from 'aurelia-framework';
 
-// @inject(preloader)
 export class App {
   constructor() {
-    this.message = 'Hello Aurelia on Netlify!';
-    this.loaded = 0;
-    // this.loader = loader;
+  }
 
+  startAudio() {
+    this.playaudio = true;
+  }
+
+  downloadAudio() {
+    this.loaded = 'downloading...';
     this.loader = preloader({
       xhrImages: false
     });
-    this.loader.add('../assets/audio1.mp3');
+    this.loader.addAudio('../assets/audio1.mp3');
     this.loader.load();
-    this.loader.on('progress',function(progress) {
-      this.loaded = progress * 100;
+    this.loader.on('progress', function(progress) {
       console.log(progress);
+      document.getElementById('downloadProgress').innerHTML = Math.ceil(progress * 100) + '%';
+    });
+    this.loader.on('complete', function() {
+      this.loaded = 'done';
+      console.log('audio loaded');
     });
   }
+
 }
