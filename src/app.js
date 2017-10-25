c7b08647065df42a3b6d40d16e23a87c87fc8634
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-25 17:16:09 +0200

    checking the cache changes

--- src/app.js
+++ src/app.js
@@ -1,27 +1,55 @@
-import preloader from 'preloader';
+// import preloader from 'preloader';
+import {PLATFORM} from 'aurelia-pal';
 
 export class App {
 
+  constructor() {
+    this.handleBodyClick = e => {
+      alert(e.target);
+    }
+
+    this.showPlayButton = false;
+    // this.cacheUpdateReady = e => {
+    //   alert(e.target);
+    // }
+  }
+
   startAudio() {
+    this.textvalue = 'iets';
     document.getElementById('audiofile').play();
   }
   downloadAudio() {
-    this.loader = preloader({
-      xhrImages: false
-    });
-    this.loader.addAudio('../assets/audio2.mp3');
-    this.loader.load();
-    this.loader.on('progress', function(progress) {
-      console.log(progress);
-      document.getElementById('downloadProgress').innerHTML = ' (' + Math.ceil(progress * 100) + '%)';
-    });
-    this.loader.on('complete', function() {
-      $('#downloadButton').hide();
-      $('#downloadProgress').hide();
-      $('#playButton').show();
-      $('#streamButton').hide();
-      this.loaded = true;
-      console.log('audio loaded');
-    });
+    // this.loader = preloader({
+    //   xhrImages: false
+    // });
+    // this.loader.addAudio('../assets/pinkfloyd5.mp3');
+    // this.loader.load();
+    // this.loader.on('progress', function(progress) {
+    //   console.log(progress);
+    //   document.getElementById('downloadProgress').innerHTML = ' (' + Math.ceil(progress * 100) + '%)';
+    // });
+    // this.loader.on('complete', function() {
+    //   $('#downloadButton').hide();
+    //   $('#downloadProgress').hide();
+    //   $('#playButton').show();
+    //   $('#streamButton').hide();
+    //   this.loaded = true;
+    //   console.log('audio loaded');
+    // });
+  }
+
+  attached() {
+
+    if (PLATFORM.global.applicationCache.status === 1) {
+      this.showPlayButton = true;
+    }
+    document.addEventListener('click', this.handleBodyClick);
+    //window.applicationCache.addEventListener('updateready', this.cacheUpdateReady, false);
+
+    //alert(this.DOM.getElementById('playButton'));
+    // this.DOM.applicationCache.addEventListener('updateready', function() {
+    //   this.DOM.applicationCache.swapCache();
+    //   alert(updateReady);
+    // }, false);         
   }
 }
